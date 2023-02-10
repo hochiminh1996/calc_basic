0dac19e84688a00d9c564c006b7b607129476672
Felippe Marques <ambrsp@hotmail.com>
2023-02-10 19:33:27 -0300

    Anotações

--- Calc/js/arquivo.js
+++ Calc/js/arquivo.js
@@ -1,21 +1,24 @@
 
 
 
-
+// adicionado um evento click a cada button que tiver uma classe .botao. Observe que foi utilizado um SelectorAll e um foreach p/ pegar os N button com classe botao
 document.querySelectorAll('.botao').forEach(function (botao) {
     botao.addEventListener('click', function () {
         insert(this.value);
     });
 })
 
-
+// função que insere os valores
 function insert(valor) {
 
     let resultado = document.querySelector("#resultado").innerHTML;
+    // valor initial do campo #resultado -> que será "" na primeira vez.
 
+    // se for c => ativa função limpar
     if (valor == "c") {
         limpar();
     } else if (valor == "<") {
+        //se for "<" ativa a função apagar
         apagar();
     } else if (["+", "-", "*", "/", "."].includes(valor)) {
         if (resultado.length > 0 && !isNaN(parseInt(resultado[resultado.length - 1]))) {
@@ -33,10 +36,14 @@ function insert(valor) {
 }
 
 
+// função para limpar o elemento com id resultado
 function limpar() {
     document.querySelector("#resultado").innerHTML = "";
 }
 
+
+
+// função que apaga o último elemento digitado
 function apagar() {
     let resul = document.querySelector("#resultado").innerHTML;
 
@@ -49,10 +56,14 @@ function apagar() {
 }
 
 
+
+// função que calcula 
 function calcular() {
     let res_calc = document.querySelector("#resultado").innerHTML;
-
+    // pegando a expressão montada no elemento #resultado : 1 + 1
 
     document.querySelector("#resultado").innerHTML = eval(res_calc);
+    // a função eval() pega uma string e realiza o calculo de acordo com o valor passado. No caso, ele leu uma string : 1+1 e, em seguinda, efetuou o cálculo e inseriu no nosso elemento resultado
+
 }
 
