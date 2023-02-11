ec6c9067ef63b4bcfb4c6a4cc9457b8d742748b9
Felippe Marques <ambrsp@hotmail.com>
2023-02-10 22:36:49 -0300

    layout: modificações

--- Calc/js/arquivo.js
+++ Calc/js/arquivo.js
@@ -23,10 +23,34 @@ function insert(valor) {
         //Se o valor do evento ativado for "<" ativa a função apagar
         apagar();
     } else if (["+", "-", "*", "/", "."].includes(valor)) {
+        /*
+           Aqui, a variável valor é comparada com um array de operações matemáticas permitidas ("+", "-", "*", "/", "."). Se o valor for encontrado no array, o código dentro do if será executado.
+
+        
+        */
+
 
         if (resultado.length > 0 && !isNaN(parseInt(resultado[resultado.length - 1]))) {
-            resultado += valor;
-            document.querySelector("#resultado").innerHTML = resultado;
+            /* 
+                !isNaN(parseInt(resultado[resultado.length - 1]) verifica se o último valor é um número, já que é uma negação de !NaN.
+
+                isNaN => se verifica se não é um número
+                !isNaN => verifica se é um número
+
+                Faça 1º primeiro a operação, por último o sinal !
+
+                !IsNaN(10) => TRUE, É UM NÚMERO
+                !IsNaN("*") => FALSE, É UM NaN
+
+             */
+
+
+            /*
+                Se o valor do btn acionado tiver algum dessas valores "+", "-", "*", "/", ".", e se tiver tamanho >0 (ou seja, não vazio) E o último valor da string for um número, AI SIM ELE IRÁ PERMITIR REPETIR OS OPERADORES ARITMÉTICOS. Caso contrário, ele insere apenas uma vez
+            */    
+            resultado += valor; // resultado recebe o valor já existente e adiciona um sinal matemática da operação escolhida
+
+            document.querySelector("#resultado").innerHTML = resultado;//adiciona o sinal ao visor de resultado
         }
 
     } else {
